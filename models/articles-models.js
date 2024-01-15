a9f1eb4f956d8700f2acab7a9e968e72a798e572
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-15 16:59:31 +0000

    add model function for GET /api/articles endpoint

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,5 +1,16 @@
 const db = require("../db/connection.js");
 
+exports.fetchArticles = async () => {
+  const result = await db.query(
+    `SELECT articles.article_id, title, topic, articles.author, articles.created_at, articles.votes, article_img_url, CAST(COUNT(comment_id) AS INT) AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC`
+  );
+  return result.rows;
+};
+
 exports.fetchArticleById = async (article_id) => {
   const regex = /^[0-9]+$/;
   if (regex.test(article_id)) {
