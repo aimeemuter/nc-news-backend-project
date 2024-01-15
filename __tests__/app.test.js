7a99bba9681069b32eba9f950fac831d2c5c2ab7
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-15 11:57:22 +0000

    add test for invalid endpoint and /api/topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,27 @@ const testData = require("../db/data/test-data/index.js");
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
+describe("invalid endpoint url", () => {
+  it("404: responds with not found if endpoint not hit", async () => {
+    const response = await request(app).get("/nonsense").expect(404);
+    const { status } = response;
+    expect(status).toBe(404);
+  });
+});
+
 describe("app.js", () => {
-  describe("GET /api/topics", () => {
-    it("", () => {});
+  describe("/api/topics", () => {
+    it("GET 200: responds to the client with an array of topics", async () => {
+      const response = await request(app).get("/api/topics").expect(200);
+      const { body } = response;
+      const { topics } = body;
+      expect(topics.length).toBe(3);
+      topics.forEach((topic) => {
+        expect(topic.hasOwnProperty("description"));
+        expect(typeof topic.description).toBe("string");
+        expect(topic.hasOwnProperty("slug"));
+        expect(typeof topic.slug).toBe("string");
+      });
+    });
   });
 });
