85086095e9bb952bee94bfacf7362d984b2ca19b
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-15 14:45:04 +0000

    add a GET /api/articles/:article_id endpoint

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const {
   getApiInfo,
   getTopics,
 } = require("./controllers/topics-controllers.js");
+const { getArticleById } = require("./controllers/articles-controllers.js");
 
 app.get("/api", getApiInfo);
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use((error, request, response, next) => {
+  if (error.status && error.message) {
+    response.status(error.status).send({ message: error.message });
+  }
+});
 
 module.exports = app;
