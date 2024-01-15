986fa8b977286c03da02f77491e0afa49390ee88
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-15 11:03:48 +0000

    setup the app.test.js file ready for GET /api/topics TDD

---
+++ __tests__/app.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const app = require("../app.js");
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed.js");
+const testData = require("../db/data/test-data/index.js");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("app.js", () => {
+  describe("GET /api/topics", () => {
+    it("", () => {});
+  });
+});
