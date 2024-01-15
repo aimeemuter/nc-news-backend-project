bfa324122c415ecdbd2d58fcfb13ec0789bce259
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-15 12:40:50 +0000

    add getApiInfo controller

--- controllers/topics-controllers.js
+++ controllers/topics-controllers.js
@@ -1,4 +1,9 @@
 const { fetchTopics } = require("../models/topics-models.js");
+const apiInfo = require("../endpoints.json");
+
+exports.getApiInfo = async (request, response, next) => {
+  response.status(200).send({ apiInfo });
+};
 
 exports.getTopics = async (request, response, next) => {
   const topics = await fetchTopics();
