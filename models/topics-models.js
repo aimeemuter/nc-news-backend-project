4d64384167f65820c26e71338b8af754762d35d3
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-19 12:25:16 +0000

    add a model function for the POST /api/topics endpoint

--- models/topics-models.js
+++ models/topics-models.js
@@ -4,3 +4,20 @@ exports.fetchTopics = async () => {
   const result = await db.query(`SELECT * FROM topics`);
   return result.rows;
 };
+
+exports.insertTopic = async (topicToInsert) => {
+  const { slug, description } = topicToInsert;
+  if (description) {
+    const result = await db.query(
+      `INSERT INTO topics (slug, description) VALUES ($1, $2) RETURNING *`,
+      [slug, description]
+    );
+    return result.rows[0];
+  } else {
+    const result = await db.query(
+      `INSERT INTO topics (slug) VALUES ($1) RETURNING slug`,
+      [slug]
+    );
+    return result.rows[0];
+  }
+};
