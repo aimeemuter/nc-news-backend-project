f7211fc2346740a4c8f48c7368d414e08b79f743
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-19 05:34:13 +0000

    add a model function for the PATCH /api/comments/:comment_id endpoint

--- models/comments-models.js
+++ models/comments-models.js
@@ -3,3 +3,24 @@ const db = require("../db/connection.js");
 exports.removeComment = async (comment_id) => {
   await db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id]);
 };
+
+exports.updateComment = async (comment_id, patchData) => {
+  if (
+    typeof patchData.inc_votes !== "number" &&
+    patchData.inc_votes !== undefined
+  ) {
+    return Promise.reject({
+      status: 400,
+      message: "The value for inc_votes must be a number",
+    });
+  } else {
+    const votesAdjustment = patchData.inc_votes || 0;
+    const result = await db.query(
+      `UPDATE comments 
+      SET votes = votes + $1 
+      WHERE comment_id = $2 RETURNING *`,
+      [votesAdjustment, comment_id]
+    );
+    return result.rows[0];
+  }
+};
