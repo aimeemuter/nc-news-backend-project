f22830195dbd34f4ffab5d17641e63174f2ea006
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-19 14:47:24 +0000

    set comments to be deleted if the article they relate to is deleted

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -36,7 +36,7 @@ const seed = async ({ topicData, userData, articleData, commentData }) => {
       CREATE TABLE comments (
         comment_id SERIAL PRIMARY KEY,
         body VARCHAR NOT NULL,
-        article_id INT REFERENCES articles(article_id) NOT NULL,
+        article_id INT REFERENCES articles(article_id) ON DELETE CASCADE NOT NULL,
         author VARCHAR REFERENCES users(username) NOT NULL,
         votes INT DEFAULT 0 NOT NULL,
         created_at TIMESTAMP DEFAULT NOW()
