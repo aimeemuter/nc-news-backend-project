eee58bed2ba4c1723512564da7e441d6ed62ceba
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2024-01-19 05:33:53 +0000

    add a controller function for the PATCH /api/comments/:comment_id endpoint

--- controllers/comments-controllers.js
+++ controllers/comments-controllers.js
@@ -1,4 +1,4 @@
-const { removeComment } = require("../models/comments-models");
+const { removeComment, updateComment } = require("../models/comments-models");
 const { fetchCommentByCommentId } = require("../utils/app-utils");
 
 exports.deleteComment = async (request, response, next) => {
@@ -7,6 +7,21 @@ exports.deleteComment = async (request, response, next) => {
     await fetchCommentByCommentId(comment_id);
     await removeComment(comment_id);
     response.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.patchComment = async (request, response, next) => {
+  const { comment_id } = request.params;
+  const patchData = request.body;
+  try {
+    const promisesArray = await Promise.all([
+      updateComment(comment_id, patchData),
+      fetchCommentByCommentId(comment_id),
+    ]);
+    const comment = promisesArray[0];
+    response.status(200).send({ comment });
   } catch (error) {
     next(error);
   }
