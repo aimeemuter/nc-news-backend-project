6167aa103a6c6ff28bbf83906cf1b737139d3c4a
Aimée Muter <123538805+aimeemuter@users.noreply.github.com>
2026-01-20 08:37:41 +0000

    update image url

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,38 +1,38 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
+    username: "tickle122",
+    name: "Tom Tickle",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953'
+      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
+    username: "grumpy19",
+    name: "Paul Grump",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013'
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
+    username: "happyamy2016",
+    name: "Amy Happy",
     avatar_url:
-      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
+      "https://static.wikia.nocookie.net/p__/images/b/b8/Little_Miss_Sunshine.jpeg/revision/latest/thumbnail/width/360/height/360?cb=20250803204151&path-prefix=protagonist",
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
+    username: "cooljmessy",
+    name: "Peter Messy",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002'
+      "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
   },
   {
-    username: 'weegembump',
-    name: 'Gemma Bump',
+    username: "weegembump",
+    name: "Gemma Bump",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553'
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553",
   },
   {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
+    username: "jessjelly",
+    name: "Jess Jelly",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141'
-  }
+      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
+  },
 ];
